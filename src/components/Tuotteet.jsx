96767e47be74224421664c40061922f97da333fe
Alex Lindholm <alexlindholm02@gmail.com>
2023-11-06 13:10:41 +0200

    rest

--- src/components/Tuotteet.jsx
+++ src/components/Tuotteet.jsx
@@ -1,7 +1,43 @@
+import { useEffect, useState } from "react";
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-material.css';
+
 export default function Tuotteet() {
+
+const [vaatteet, setVaatteet]=useState([]);
+
+const rest_url= 'http://localhost:8080/vaatteet'
+useEffect(()=>{getVaatteet()}, [])
+
+
+    const columns= [
+        {field: 'tyyppi'},
+        {field: 'vari'},
+        {field: 'koko'},
+        {field: 'hinta'},
+    ]
+    const getVaatteet=()=>{
+    fetch(rest_url)
+    .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+    .then(responseData=> {
+        setVaatteet(responseData.vaatteet)
+    })}
     return(
-        <>
-        <h1>Tuotteemme:</h1>
-        </>
+        <div className="ag-theme-material"
+        style={{height: '700px', width: '70%', margin: 'auto'}}>
+            <AgGridReact 
+            rowData={vaatteet}
+            columnDefs={columns}
+            >
+                
+            </AgGridReact>
+            </div>
     )
+    
 }
