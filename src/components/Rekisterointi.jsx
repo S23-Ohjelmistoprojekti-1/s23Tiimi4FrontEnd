f515f7ba1bbd40b5cd3d939c1b6219cf923b6aa1
Alex Lindholm <alexlindholm02@gmail.com>
2023-11-22 13:16:48 +0200

    rekisteröinti kentät päivitetty

--- src/components/Rekisterointi.jsx
+++ src/components/Rekisterointi.jsx
@@ -1,12 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Snackbar } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 export default function Rekisterointi() {
   const [asiakas, setAsiakas] = useState({
     etunimi: '',
     sukunimi: '',
+    kotiosoite:'' ,
+    postinumero:'',
+    postitoimipaikka: '',
     puhelinnumero: '',
     email: '',
   });
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -23,12 +29,17 @@ export default function Rekisterointi() {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(asiakas),
+
       });
   
       if (response.ok) {
-        console.log('Asiakas successfully added to the database.');
+        alert('Rekisteröinti onnistui!');
+        navigate('/');
+                
+               
+
       } else {
-        console.error('Failed to add Asiakas to the database.');
+        alert('Failed to add Asiakas to the database.');
       }
     } catch (error) {
       console.error('Error:', error);
@@ -61,7 +72,36 @@ export default function Rekisterointi() {
           />
         </label>
         <br />
-
+        <label>
+        Kotiosoite:
+          <input
+            type="text"
+            name="kotiosoite"
+            value={asiakas.kotiosoite}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+        Postinumero:
+          <input
+            type="text"
+            name="postinumero"
+            value={asiakas.postinumero}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+        Postitoimipaikka:
+          <input
+            type="text"
+            name="postitoimipaikka"
+            value={asiakas.postitoimipaikka}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
         <label>
           Puhelinnumero:
           <input
@@ -86,6 +126,11 @@ export default function Rekisterointi() {
 
         <button type="submit">Rekisteröi</button>
       </form>
+      <div>
+      <h3>Haluatko poistaa tietosi?</h3>
+      <h4>Ota yhteyttä:</h4>
+      <p>pertti.peruna@hotmail.com</p>
+      </div>
     </>
   );
 }
