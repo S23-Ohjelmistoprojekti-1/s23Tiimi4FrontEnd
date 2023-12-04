8c5e96d70cf9426b23bc1c0bcd7819b67e16888d
SpookySkoops <rautiainen.oskarir@gmail.com>
2023-12-04 11:44:20 +0200

    deployment yritys 4

--- src/components/Rekisterointi.jsx
+++ src/components/Rekisterointi.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { Snackbar } from '@mui/material';
+import { useState } from 'react';
+//import { Snackbar } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
 export default function Rekisterointi() {
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 
+// https://vitejs.dev/config/
 export default defineConfig({
- // base: '/s23Tiimi4FrontEnd/',
   plugins: [react()],
-  test: {
-  globals: true,
-  environment: 'jsdom',
-  },
-  })
+})
+
