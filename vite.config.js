1329c1a5825e5a9ecb35d58e22170b8cb1f8041c
SpookySkoops <rautiainen.oskarir@gmail.com>
2023-12-04 11:18:47 +0200

    deployment yritys 2

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  base: '/s23Tiimi4FrontEnd/',
+ // base: '/s23Tiimi4FrontEnd/',
   plugins: [react()],
   test: {
   globals: true,
